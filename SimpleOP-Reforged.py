dd5da417821e304d650d837ee306eb71973324ae
GamerNoTitle <bili33@87ouo.top>
2020-07-08 10:42:49 +0800

    修改提示

--- SimpleOP-Reforged.py
+++ SimpleOP-Reforged.py
@@ -2,13 +2,14 @@
 import time
 import re
 import sys
+sys.path.append('plugins/')
+from OnlinePlayerAPI import check_online
 
 msg='''
 §6======== SimpleOP Reforged ========
 §5一个由佛冷的SimpleOP魔改而来的小插件
 §5魔改的目的是供自己使用，万物皆可魔改~
 §5--GamerNoTitle
-§b需要OnlinePlayerAPI支持~
 §6!!op 可以获取OP权限（无权限要求§4危险§6）
 §6!!restart 可以重启服务器（有10s倒计时）
 §6!!stop 可以关闭服务器（有10s倒计时）
@@ -31,15 +32,20 @@ def on_info(server, info):
     message=info.content.split()
     if info.content == '!!sr':
         server.tell(info.player, msg)
-
-    if message[0]=='!!sr':            
+    
+    if message[0]=='!!sr':
+        print(len(message))
+        print(message[2])
         if info.is_player and message[1] == 'where' and len(message)==3:
+            player_for_search=message[2]
             try:
                 position = process_coordinate(re.search(r'\[.*\]', server.rcon_query('data get entity {} Pos'.format(player_for_search))).group())
-                where='Player §b{} §rat §6{} §6{} §6{}'.format(player_for_search,int(position[0]),int(position[1]),int(position[2]))
+                where='玩家§b{}§r在[§6{} §6{} §6{}§r]'.format(player_for_search,int(position[0]),int(position[1]),int(position[2]))
                 server.tell(info.player, where)
+                server.tell(player_for_search, '玩家{}正在寻找你,你将会被应用15秒的高亮效果'.format(info.player))
+                server.execute('effect give {} minecraft:glowing 15 0 true'.format(player_for_search))
             except:
-                server.tell(info.player,'Player §b{} §ris not online now!'.format(player_for_search))
+                server.tell(info.player,'玩家§b{}§r不在线'.format(player_for_search))
             
         if info.is_player and message[1] == 'sp':
             position = process_coordinate(re.search(r'\[.*\]', server.rcon_query('data get entity {} Pos'.format(info.player))).group())
